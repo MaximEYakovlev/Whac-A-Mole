f36264368a1b52de1893858286873b0f62e38d06
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-17 12:00:00 +0300

    added random selection of game field squares, set the squares in motion

---
+++ app.js
@@ -0,0 +1,21 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+
+let result = 0;
+
+function randomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+}
+
+function moveMole() {
+  let timerId = null;
+  timerId = setInterval(randomSquare, 500);
+}
+moveMole();
