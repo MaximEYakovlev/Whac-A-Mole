f2f6b48c8e0d74ebc2ec87dffa832c36ea851d7f
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-18 12:17:03 +0300

    added functionality to hit the mole

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPosition;
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -12,10 +13,23 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
 }
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
 
 function moveMole() {
   let timerId = null;
-  timerId = setInterval(randomSquare, 500);
+  timerId = setInterval(randomSquare, 1000);
 }
+
 moveMole();
